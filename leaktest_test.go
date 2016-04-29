a124ea2557816a9b1c596ca7dcfbb2bf433c9664
fortytw2 <fortytw2@gmail.com>
2016-04-29 11:15:06 +0200

    add several tests cases from golang/go#12989

--- leaktest_test.go
+++ leaktest_test.go
@@ -2,6 +2,7 @@ package leaktest
 
 import (
 	"fmt"
+	"sync"
 	"testing"
 	"time"
 )
@@ -16,18 +17,62 @@ func (tr *testReporter) Errorf(format string, args ...interface{}) {
 	tr.msg = fmt.Sprintf(format, args)
 }
 
-func TestCheck(t *testing.T) {
-	checker := &testReporter{}
-
-	snapshot := Check(checker)
-	go func() {
+var leakyFuncs = []func(){
+	// Infinite for loop
+	func() {
 		for {
 			time.Sleep(time.Second)
 		}
-	}()
+	},
+	// Select on a channel not referenced by other goroutines.
+	func() {
+		c := make(chan struct{}, 0)
+		select {
+		case <-c:
+		}
+	},
+	// Blocked select on channels not referenced by other goroutines.
+	func() {
+		c := make(chan struct{}, 0)
+		c2 := make(chan struct{}, 0)
+		select {
+		case <-c:
+		case c2 <- struct{}{}:
+		}
+	},
+	// Blocking wait on sync.Mutex that isn't referenced by other goroutines.
+	func() {
+		var mu sync.Mutex
+		mu.Lock()
+		mu.Lock()
+	},
+	// Blocking wait on sync.RWMutex that isn't referenced by other goroutines.
+	func() {
+		var mu sync.RWMutex
+		mu.RLock()
+		mu.Lock()
+	},
+	func() {
+		var mu sync.Mutex
+		mu.Lock()
+		c := sync.NewCond(&mu)
+		c.Wait()
+	},
+}
 
-	snapshot()
-	if !checker.failed {
-		t.Errorf("didn't catch sleeping goroutine")
+func TestCheck(t *testing.T) {
+
+	// this works because the running goroutine is left running at the
+	// start of the next test case - so the previous leaks don't affect the
+	// check for the next one
+	for i, fn := range leakyFuncs {
+		checker := &testReporter{}
+		snapshot := Check(checker)
+		go fn()
+
+		snapshot()
+		if !checker.failed {
+			t.Errorf("didn't catch sleeping goroutine, test #%d", i)
+		}
 	}
 }
